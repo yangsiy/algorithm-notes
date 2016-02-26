23421c619a41da35e2f48fe2ecd65f684d83c772
Siyu Yang <yangsiyu1992@gmail.com>
2016-02-27 00:27:12 +0800

    #20 c++ solution

---
+++ 20_valid-parentheses/20_valid-parentheses.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int transform(char ch)
+    {
+        if (ch == '(') return 1;
+        if (ch == '[') return 2;
+        if (ch == '{') return 3;
+        if (ch == ')') return -1;
+        if (ch == ']') return -2;
+        if (ch == '}') return -3;
+    }
+
+    bool isValid(string s) {
+        
+        int len = s.length();
+        int stack[len+1],pos;
+        int k;
+        
+        pos = -1;
+        for (int i = 0; i < len; i++)
+        {
+            k = transform(s[i]);
+            if (k > 0)
+            {
+                pos++;
+                stack[pos] = k;
+                continue;
+            }
+            if (pos < 0) return false;
+            if (stack[pos]+k != 0) return false;
+            pos--;
+        }
+        
+        if (pos >= 0) return false;
+        return true;
+    }
+};
